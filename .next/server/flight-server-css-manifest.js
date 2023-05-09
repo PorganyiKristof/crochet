4371dfbabad58be4f7b1c5bd6d91c67a94957225
PorganyiKristof <75934852+PorganyiKristof@users.noreply.github.com>
2023-05-09 19:03:05 +0200

    deploy app

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_CSS_MANIFEST={"cssImports":{"D:\\programok\\Xampp\\htdocs\\NextJs\\crochet\\app\\layout.jsx":["D:\\programok\\Xampp\\htdocs\\NextJs\\crochet\\styles\\globals.css"]},"cssModules":{"D:\\programok\\Xampp\\htdocs\\NextJs\\crochet\\app\\page":["D:\\programok\\Xampp\\htdocs\\NextJs\\crochet\\styles\\globals.css"]}}
