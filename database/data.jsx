ff04845c3c0a48467889c9f5e156fd121ac59af8
PorganyiKristof <75934852+PorganyiKristof@users.noreply.github.com>
2023-05-13 23:20:32 +0200

    jpg fix

--- database/data.jsx
+++ database/data.jsx
@@ -1,7 +1,7 @@
 import test1 from "@public/assets/images/343016261_764547991740663_7145802567922255948_n.jpg";
 import test2 from "@public/assets/images/343016261_764547991740663_7145802567922255948_n.png";
 import test3 from "@public/assets/images/343082287_5692679047503334_5048110264889831882_n.jpg";
-import zumzum from "@public/assets/images/zumzum.jpg";
+import zumzum from "@public/assets/images/zumzum.JPG";
 
 const data = {
   crochets: [
